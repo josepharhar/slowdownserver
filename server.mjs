512c6cce06635d201988f2a05d69a7ebeb6e4775
Joey Arhar <josepharhar@gmail.com>
2025-01-06 23:57:44 -0800

    omg it works

--- server.mjs
+++ server.mjs
@@ -1,5 +1,8 @@
+import {promisify} from 'node:util';
 import http from 'node:http';
 import fs from 'node:fs/promises';
+import child_process from 'node:child_process';
+const exec = promisify(child_process.exec);
 
 const server = http.createServer(async (req, res) => {
   console.log(`${req.method} ${req.url}`);
@@ -21,6 +24,20 @@ const server = http.createServer(async (req, res) => {
     } else if (url.pathname = '/slowmedownload') {
       const downloadUrl = url.searchParams.get('url');
       console.log('downloading url: ' + downloadUrl);
+      console.log('running yt-dlp...');
+      const { stdout, stderr } = await exec(
+        `yt-dlp -o asdf -x --audio-format mp3 "${downloadUrl}"`);
+      console.log('stdout:');
+      console.log(stdout);
+      console.log('stderr:');
+      console.log(stderr);
+      console.log('going to reply with asdf.mp3');
+      res.writeHead(200, {
+        'content-type': 'audio/mpeg',
+        'content-disposition': 'filename=asdf.mp3'
+      });
+      const contents = await fs.readFile('asdf.mp3');
+      res.end(contents);
     }
   } catch (e) {
     res.writeHead(500, {'content-type': 'text/plain'});
